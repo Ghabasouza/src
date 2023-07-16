eb087ff02686f900a0b0e2b9ddfc9d880f1be920
Daniel Ferrreira <85563235+DanielMarinhoFerreira@users.noreply.github.com>
2023-07-15 21:56:28 -0300

    correções

--- app.py
+++ app.py
@@ -6,4 +6,6 @@ import json
   
 
 teste = FileArq('fatvenda.xml')
-t = teste.module_arq(teste.open_arq())
+#t = teste.module_arq(teste.open_arq())
+tes = teste.menu_title_sub(teste.open_arq())
+print(tes)
